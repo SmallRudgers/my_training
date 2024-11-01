9fecc147643ddba01cd5ba612a09d2e6099c1b5f
SmallRudgers <dk.inst38@gmail.com>
2024-11-02 03:09:19 +0800

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,6 @@
+completed_homework = 12
+hours_spent = 1.5
+Course_name = "Python"
+one_task_time = hours_spent / completed_homework
+
+print(Course_name,", всего задач:",completed_homework,', затрачено часов:',hours_spent, ', среднее время выполнения: ', one_task_time,' часа.', sep='')
