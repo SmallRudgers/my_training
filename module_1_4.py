53949b1e080f99d0027afc78a4b0bdca2cbfafde
SmallRudgers <dk.inst38@gmail.com>
2024-11-06 05:06:39 +0800

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,13 @@
+my_string = input("Введите что вам угодно: ")
+
+print("Кол-во символов в введенyой строке:", len(my_string))
+
+print("Перевод в верхний регистр: ", my_string.upper())
+
+print("Перевод в нижний регистр: ", my_string.lower())
+
+print("Без пробелов: ", my_string.replace( ' ', ''))
+
+print("Первый символ: ",my_string[0])
+
+print("Последний символ: ", my_string[-1])
