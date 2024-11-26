c8718a17fc01fe612241fb625b3f12cbf2478b8a
SmallRudgers <dk.inst38@gmail.com>
2024-11-26 17:42:59 +0800

    Update Module_2_3.py

--- Module_2_3.py
+++ Module_2_3.py
@@ -1,12 +1,15 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 n = 0
-len_list = len(my_list)
 new_list = []
 
 while my_list [n] >= 0:
-    new_list.append(my_list[n])
+
+    if my_list [n] > 0:
+        new_list.append(my_list[n])
+
     n += 1
-    if n == len_list:
+
+    if n == len(my_list):
         break
 
 print(new_list)
