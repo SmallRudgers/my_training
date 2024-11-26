acce9ca7962a89997340e408e41329bf5500ede9
SmallRudgers <dk.inst38@gmail.com>
2024-11-26 17:33:36 +0800

    Add files via upload

---
+++ Module_3_2.py
@@ -0,0 +1,32 @@
+def send_email(message,recipient,*,sender="university.help@gmail.com"):
+
+    check = 0
+    fail_check = ['.com', '.ru', '.net']
+
+    for i in fail_check:
+
+        if i in sender[len(sender) - 4:]:
+            check += 1
+
+        if i in recipient[len(recipient) - 4:]:
+            check += 1
+
+    if '@' in sender and check == 2 and '@' in recipient:
+
+        if sender == recipient:
+            print("Нельзя отправить письмо самому себе!")
+
+        elif sender == "university.help@gmail.com":
+            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+
+        else:
+            print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+
+    else:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
